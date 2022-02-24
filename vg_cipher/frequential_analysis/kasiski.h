0a48159a4f7cd8e112f1a12146951e0c025d31d3
X <43837752+Equis-X@users.noreply.github.com>
2022-02-24 16:28:25 -0400

    Delete vg_cipher directory

--- vg_cipher/frequential_analysis/kasiski.h
+++
@@ -1,6 +0,0 @@
-#include <map>
-#include <set>
-#include <vector>
-#include <string>
-
-std::vector<unsigned> findKey(int, int);
--- vg_cipher/frequential_analysis/keylength.h
+++
@@ -1,9 +0,0 @@
-#include <set>
-#include <map>
-#include <string>
-
-void findRepeatedSubstrings(int, std::map<std::string, unsigned> &, std::set<unsigned> &);
-
-unsigned findKeyLength(int);
-
-void findDistanceDivisors(unsigned, std::map<unsigned, unsigned> &);
